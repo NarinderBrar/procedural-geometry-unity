a0a89aa25e0dea4b862ed246f931599aca1dc3e7
Narinder <narinder.singh@chitkara.edu.in>
2022-05-25 07:45:46 +0530

    backup

---
+++ Assets/ProcGen.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcGen : MonoBehaviour
+{
+    void Awake()
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "Procedural Quad";
+
+        List<Vector3> points = new List<Vector3>()
+        {
+            new Vector3(-1,1),
+            new Vector3(1,1),
+            new Vector3(-1,-1),
+            new Vector3(1,-1),
+        };
+
+        int[] triIndices = new int[]
+            {
+                1,0,2,
+                3,1,2
+            };
+
+        List<Vector3> normals = new List<Vector3>()
+        {
+            new Vector3(0,0,1),
+            new Vector3(0,0,1),
+            new Vector3(0,0,1),
+            new Vector3(0,0,1),
+        }
+
+        mesh.SetVertices(points);
+        mesh.triangles = triIndices;
+
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().sharedMesh = mesh;
+    }
+}
---
+++ Assets/Scripts/ProcGen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcGen : MonoBehaviour
+{
+    void Awake()
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "Procedural Quad";
+
+        List<Vector3> points = new List<Vector3>()
+        {
+            new Vector3(-1,1),
+            new Vector3(1,1),
+            new Vector3(-1,-1),
+            new Vector3(1,-1),
+        };
+
+        int[] triIndices = new int[]
+            {
+                1,0,2,
+                3,1,2
+            };
+
+        mesh.SetVertices(points);
+        mesh.triangles = triIndices;
+
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().sharedMesh = mesh;
+    }
+}
