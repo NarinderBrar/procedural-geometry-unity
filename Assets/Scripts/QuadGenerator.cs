29ec6842a26069b882e389408bb422d0508425c5
Narinder Pal Singh <nsidhubrar@gmail.com>
2022-06-11 16:09:24 +0530

    normals

---
+++ Assets/Scripts/QuadGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadGenerator : MonoBehaviour
+{
+    void Awake()
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "Procedural Quad";
+
+        List<Vector3> points = new List<Vector3>()
+        {
+            new Vector3(-0.5f,0.5f),
+            new Vector3(0.5f,0.5f),
+            new Vector3(-0.5f,-0.5f),
+            new Vector3(0.5f,-0.5f),
+        };
+
+        int[] triIndices = new int[]
+            {
+                1,0,2,
+                3,1,2
+            };
+
+        List<Vector2> uvs = new List<Vector2>()
+        {
+            new Vector2(1,1),
+            new Vector2(0,1),
+            new Vector2(1,0),
+            new Vector2(0,0),
+        };
+
+        List<Vector3> normals = new List<Vector3>()
+        {
+            new Vector3(0,0,1),
+            new Vector3(0,0,1),
+            new Vector3(0,0,1),
+            new Vector3(0,1,0)
+        };
+
+        mesh.SetVertices(points);
+        mesh.SetNormals(normals);
+        mesh.SetUVs(0, uvs);
+
+        mesh.triangles = triIndices;
+
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().sharedMesh = mesh;
+    }
+}
---
+++ Assets/Scripts/QuadRing.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadRing : MonoBehaviour
+{
+    [Range(0.01f, 2)]
+    [SerializeField] float radiusInner;
+
+    [Range(0.01f, 2)]
+    [SerializeField] float thickness;
+
+    float RadiusOuter => radiusInner + thickness;
+
+    [Range(3, 256)]
+    [SerializeField] int angularSegments = 3;
+
+	private void OnDrawGizmos()
+	{
+        Gizmos.DrawWireSphere(transform.position, radiusInner);
+        Gizmos.DrawWireSphere(transform.position, RadiusOuter);
+    }
+
+    const float TAU = 6.28318530718f;
+    public static void DrawWireCircle(Vector3 pos, Quaternion rot, float radius, int detail = 32)
+    {
+        Vector2[] points3D = new Vector2[detail];
+
+        for (int i = 0; i < detail; i++)
+        {
+            float t = i / (float) detail;
+            float angleRad = t * TAU;
+
+            Vector2 point2D = new Vector2(
+                Mathf.Cos(angleRad) * radius,
+                Mathf.Sin(angleRad) * radius
+                );
+
+            points3D[i] = pos+ rot * point2D;
+        }
+
+		for (int i = 0; i < detail; i++)
+		{
+            Gizmos.DrawSphere(points3D[i], 0.02f);
+		}
+       
+
+    }
+
+	void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
